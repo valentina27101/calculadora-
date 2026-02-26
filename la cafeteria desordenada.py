8f76a3b0297be56788d978e23d27bc97eb81a98a
Juseth Porto <jusethporto2@gmail.com>
2026-02-26 18:46:41 -0500

    menu

---
+++ la cafeteria desordenada.py
@@ -0,0 +1,12 @@
+def mostrar_menu():
+    print("bienvenidos a la cafeteria")
+    print("1. cafe - 2k")
+    print("2. postre del dia - 5k")
+    print("3. agua  - 1k")
+    print("4. brownie  - 6k")
+    print("5. finalizar compra")
+
+    
+
+
+
