d873c8368733e956e22dd9d3dc6d927be52a61b1
luis suarez <luismasuarezsevilla@gmail.com>
2026-02-26 18:54:42 -0500

    hola

---
+++ la cafeteria desordenada.py
@@ -0,0 +1,21 @@
+def menu_de_cafeteria():
+    print("bienvenido a la cafeteria desordenada que desea ordenar?")
+    print("1. cafe")
+    print("2. te")
+    print("3. jugo")
+    print("4. pastel")
+    opcion = int(input("ingrese el numero de su orden: "))
+
+    if opcion == 1:
+        print("usted ha ordenado un cafe")
+
+    elif opcion == 2:
+        print("usted ha ordenado un te")
+    elif opcion == 3:
+        print("ustedd ha ordenado un jugo")
+    elif opcion == 4:
+        print("usted ha ordenado un pastel")
+    
+    menu_de_cafeteria()
+
+menu_de_cafeteria()
